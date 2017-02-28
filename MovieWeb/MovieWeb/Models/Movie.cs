77e3dcee69b7158726a77f29c0f96d29abdfaae6
Jovana Titov <jovanastar96@gmail.com>
2017-02-28 20:02:29 +0100

    prvi komit

---
+++ MovieWeb/MovieWeb/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieWeb.Models
+{
+    public class Movie
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public DateTime DateOfRelease { get; set; }
+    }
+}
